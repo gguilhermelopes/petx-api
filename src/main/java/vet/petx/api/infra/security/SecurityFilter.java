887b544aa6a3dd3ca59545964661dec506139dd7
gguilhermelopes <guilhermelopes275@gmail.com>
2023-09-28 21:39:58 -0300

    feat: set configurations to validate token and authenticate

---
+++ src/main/java/vet/petx/api/infra/security/SecurityFilter.java
@@ -0,0 +1,57 @@
+package vet.petx.api.infra.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import vet.petx.api.domain.user.User;
+import vet.petx.api.domain.user.UserRepository;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+        String jwtToken = getToken(request);
+
+        if(jwtToken != null){
+            String subject = tokenService.getSubject(jwtToken);
+            UserDetails user = userRepository.findByEmail(subject);
+
+            Authentication authentication =
+                    new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String getToken(HttpServletRequest request) {
+        String authorizationHeader = request.getHeader("Authorization");
+        if(authorizationHeader != null){
+            return authorizationHeader.replace("Bearer ", "");
+        }
+
+        return null;
+    }
+}
--- src/main/java/vet/petx/api/infra/security/TokenService.java
+++ src/main/java/vet/petx/api/infra/security/TokenService.java
@@ -3,6 +3,7 @@ package vet.petx.api.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import vet.petx.api.domain.user.User;
@@ -27,6 +28,19 @@ public class TokenService {
                     .sign(algorithm);
         } catch (JWTCreationException exception){
             throw new RuntimeException("Error on jwt token creation.", exception);
+        }
+    }
+
+    public String getSubject(String jwtToken){
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("Petx API")
+                    .build()
+                    .verify(jwtToken)
+                    .getSubject();
+        } catch (JWTVerificationException exception){
+            throw new RuntimeException("Invalid or expired jwt token.", exception);
         }
     }
 
